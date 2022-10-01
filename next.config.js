d1050c05871a813a1d98372b10c5a067cf86ff43
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-01 11:51:40 +0800

    map out env files to be used by client

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    ALGOLIA_APPLICATION_ID: process.env.ALGOLIA_APPLICATION_ID,
+    ALGOLIA_SEARCH_API_KEY: process.env.ALGOLIA_SEARCH_API_KEY,
+    CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
+    CONTENTFUL_TOKEN: process.env.CONTENTFUL_TOKEN,
+    CONTENTFUL_HOST: process.env.CONTENTFUL_HOST,
+  },
 }
