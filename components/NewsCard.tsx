0e7f40fac9586e88127d50ee977e0a211dd1ff44
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-01 15:57:48 +0800

    adds link to news card

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -1,5 +1,13 @@
-import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material'
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Link as MUILink,
+  Typography,
+} from '@mui/material'
 import { readableDate } from 'helpers'
+import Link from 'next/link'
 import type { Article } from 'types'
 
 type NewsCardProps = {
@@ -14,9 +22,11 @@ export const NewsCard: React.FC<NewsCardProps> = ({ article }) => {
         <Typography fontSize='12px' mb={1} color='blue'>
           {article?.topics?.map(({ title }) => title).join(', ')}
         </Typography>
-        <Typography color='black' fontWeight='bold'>
-          {article?.name || '---'}
-        </Typography>
+        <Link href={`/news/${article.objectID}`} passHref>
+          <MUILink fontWeight='bold' underline='none' color='black'>
+            {article?.name || '---'}
+          </MUILink>
+        </Link>
         <Box display='flex' mt={3} gap={4}>
           <Typography fontSize='12px'>
             {readableDate(article.publicationDate)}
