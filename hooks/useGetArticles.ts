0c2ca040f8a5a880f3e2f9e202682027d9664206
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-01 11:51:53 +0800

    create hook to consume service

---
+++ hooks/useGetArticles.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react'
+import AlgoliaClient from 'lib/algoliaService'
+import type { Article } from 'types'
+
+export const useGetArticles = () => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [articles, setArticles] = useState<Article[]>([])
+  const [indexDetails, setIndexDetails] = useState<any>({
+    totalArticles: 0,
+    totalPagels: 0,
+    currentPage: 0,
+  })
+
+  const listArticles = useCallback(async () => {
+    const index = AlgoliaClient.initIndex('credible_mind')
+
+    try {
+      const articles = await index.search<Article>('')
+      setIndexDetails({
+        totalArticles: articles.nbHits,
+        totalPages: articles.nbPages,
+        currentPage: articles.page,
+      })
+      setArticles(articles?.hits || [])
+    } catch (error) {
+      console.error(error)
+    }
+
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    setLoading(true)
+    listArticles()
+  }, [listArticles])
+
+  return {
+    loading,
+    articles,
+    indexDetails,
+  }
+}
