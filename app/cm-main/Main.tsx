dd141ef4aa8d0fb9f74d496c341d89d6bc5a3c37
Chester Mendoza <ronchester.mendoza@gmail.com>
2022-10-01 10:00:49 +0800

    creates skeletal layout of the main page

--- app/cm-main/Main.tsx
+++ app/cm-main/Main.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@mui/material'
+import { Box, Container, Grid, Typography } from '@mui/material'
 import { Field } from 'contentful'
 
 type MainProps = {
@@ -7,6 +7,30 @@ type MainProps = {
 
 export const Main: React.FC<MainProps> = ({ fields }) => (
   <Container>
-    <Box>Main Page</Box>
+    <Box py={2.5} px={8}>
+      <Box>Logo</Box>
+    </Box>
+    <Box display='flex' py={2}>
+      <Box>News</Box>
+      <Box ml='auto'>Drop Down</Box>
+    </Box>
+    <Box borderBottom='1px solid #afd9ea' borderTop='1px solid #afd9ea' pb={3}>
+      <Box display='flex' justifyContent='center' py={8}>
+        <Typography fontWeight='bold' fontSize={32}>
+          CredibleMind in the News
+        </Typography>
+      </Box>
+      <Grid container spacing={2}>
+        {Array.from({ length: 3 }).map((_arr, i) => (
+          <Grid item key={`grid-${i}`} xs={4}>
+            <Box>Article</Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+    <Box display='flex'>
+      <Box>Search Box</Box>
+      <Box>Resources</Box>
+    </Box>
   </Container>
 )
